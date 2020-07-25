e6b6d4fc4bb7f5690bc9ba35eba72303f4639a9d
Lisa White <whitelisab@gmail.com>
2020-07-25 11:00:39 -0600

    First commit
    Create package.json, install dependencies, add linter, set up
    babel and webpack config, and create .gitignore file.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        use: 'babel-loader',
+      },
+    ],
+  },
+};
