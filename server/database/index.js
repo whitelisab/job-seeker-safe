8335cf32d7993189b5ff8431697d615185c9b464
Lisa White <whitelisab@gmail.com>
2020-07-29 20:05:12 -0600

    Update environment files and add Procfile

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/job-seeker', { useNewUrlParser: true, useUnifiedTopology: true }).then(
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true }).then(
   () => { console.log('db connected'); },
   (err) => { console.log('error connecting to db', err); },
 );
